b999eff1aa8c85bd8ee383a3a4074194be3e3273
Hossein Mohammadi <hosseinm.developer@gmail.com>
2020-02-28 14:39:53 +0330

    feat: add state and dispatch to provider ref handler

--- src/storeModule/provider.js
+++ src/storeModule/provider.js
@@ -1,25 +1,43 @@
-import React, { useReducer, useMemo, useCallback } from "react";
+import React, {
+  useReducer,
+  useMemo,
+  useCallback,
+  useImperativeHandle,
+  forwardRef,
+  memo,
+} from "react";
 
 /**
  * @type {React.FC<{store: any,dispatchListener:()=> void}}>}
  */
-export const Provider = ({ children, store, dispatchListener }) => {
-  const { reducer, initialState, middleware } = store;
-  const [state, dispatch] = useReducer(reducer, initialState);
+export const Provider = memo(
+  forwardRef(({ children, store, dispatchListener }, ref) => {
+    const { reducer, initialState, middleware } = store;
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-  const dispatchWrapper = useCallback(
-    (...arg) => {
-      dispatch(...arg);
-      dispatchListener?.(...arg);
-    },
-    [dispatchListener],
-  );
+    const dispatchWrapper = useCallback(
+      (...arg) => {
+        dispatch(...arg);
+        dispatchListener?.(...arg);
+      },
+      [dispatchListener],
+    );
 
-  middleware && middleware({ state, initialState });
+    middleware && middleware({ state, initialState });
 
-  const value = useMemo(() => {
-    return [state, dispatchWrapper];
-  }, [state, dispatchWrapper]);
+    useImperativeHandle(
+      ref,
+      () => ({
+        state,
+        dispatch: dispatchWrapper,
+      }),
+      [dispatchWrapper, state],
+    );
 
-  return <store.Provider value={value} children={children} />;
-};
+    const value = useMemo(() => {
+      return [state, dispatchWrapper];
+    }, [state, dispatchWrapper]);
+
+    return <store.Provider value={value} children={children} />;
+  }),
+);
